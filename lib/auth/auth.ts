4b6254982046cd1c4cc7bc47b85a21eb94df1cad
Parsa <realparsasedighi@gmail.com>
2025-08-30 16:46:42 +0330

    add more origins to better-auth

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -4,21 +4,35 @@ import { db } from "@/lib/db";
 import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
-    database: prismaAdapter(db, {
-        provider: "postgresql",
-    }),
-    plugins: [nextCookies()],
-    emailAndPassword: {
-        enabled: true,
+  database: prismaAdapter(db, {
+    provider: "postgresql",
+  }),
+  plugins: [nextCookies()],
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     },
-    socialProviders: {
-        github: {
-            clientId: process.env.GITHUB_CLIENT_ID as string,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-        }
-    },
-    cors: {
-        origin: ["http://localhost:3000", "https://www.crosscopy.ir"]
-    },
-    trustedOrigins: ["http://localhost:3000", process.env.NETWORK_TRUSTED_ORIGIN!],
+  },
+  cors: {
+    origin: [
+      "http://localhost:3000",
+      "https://crosscopy.ir",
+      "https://www.crosscopy.ir",
+    ],
+    credentials: true,
+  },
+  cookies: {
+    domain: ".crosscopy.ir",
+    secure: true,
+    sameSite: "lax",
+  },
+  trustedOrigins: [
+    "http://localhost:3000",
+    process.env.NEXT_PUBLIC_BASE_URL!,
+    process.env.NETWORK_TRUSTED_ORIGIN!,
+  ],
 });
